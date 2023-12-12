37443e8f9224a9cad24e6c766816e47e4812e8aa
ahermelo <agustina.hermelo@vtex.com.br>
2023-12-12 19:01:41 -0300

    Entrega 3

--- dags/dag_stocks_info.py
+++ dags/dag_stocks_info.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from scripts.main import main
+
 
 
 default_args = {
@@ -13,7 +13,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="stocks_etl_dag",
+    dag_id="dag_stocks_info",
     start_date=datetime(2023, 12, 1),
     catchup=False,
     schedule_interval="@daily",
@@ -30,8 +30,8 @@ with DAG(
     )
 
     extract_stocks_data_task = BashOperator(
-        task_id="extract_stocks_data",
-        bash_command="python scripts/main.py"
+        task_id="extract_and_load_stocks_data",
+        bash_command="python scripts/stocks_etl_main.py"
     )
 
     create_tables_task >> extract_stocks_data_task
